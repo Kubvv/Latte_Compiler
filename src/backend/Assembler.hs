5f9b9ef0c78b70484d2b477d6bb46d17e56fe2c4
Kubv <jakub.grondziowski@gmail.com>
2022-12-06 14:41:31 +0100

    feat(backend): complete assembler data structure

--- src/backend/Assembler.hs
+++ src/backend/Assembler.hs
@@ -43,26 +43,24 @@ data Stmt =
   deriving (Eq, Show)
 
 data Register = 
-    RDX 
-  | EDX 
-  | DL
-  | RCX 
-  | ECX 
-  | CL
-  | RAX 
+    RAX 
   | EAX 
   | AL
-  | RBP
-  | RSP
-  | RSI 
-  | ESI 
-  | SIL
+  | RBX 
+  | EBX 
+  | BL 
+  | RCX
+  | ECX
+  | CL
+  | RDX
+  | EDX
+  | DL
   | RDI
   | EDI 
   | DIL
-  | RBX 
-  | EBX 
-  | BL  
+  | RSI 
+  | ESI 
+  | SIL 
   | R8  
   | R8D 
   | R8B
@@ -87,9 +85,118 @@ data Register =
   | R15 
   | R15D 
   | R15B
+  | RBP
+  | RSP
   deriving (Eq, Ord, Show)
 
+increase :: Register -> Register
+increase AL = RAX
+increase EAX = RAX
+increase RAX = RAX
+increase BL = RBX
+increase EBX = RBX
+increase RBX = RBX
+increase CL = RCX
+increase ECX = RCX
+increase RCX = RCX
+increase DL = RDX
+increase EDX = RDX
+increase RDX = RDX
+increase EDI = RDI
+increase RDI = RDI
+increase DIL = RDI
+increase ESI = RSI
+increase RSI = RSI
+increase SIL = RSI
+increase R8B = R8
+increase R8D = R8
+increase R8 = R8
+increase R9B = R9
+increase R9D = R9
+increase R9 = R9
+increase R10B = R10
+increase R10D = R10
+increase R10 = R10
+increase R11B = R11
+increase R11D = R11
+increase R11 = R11
+increase R12B = R12
+increase R12D = R12
+increase R12 = R12
+increase R13B = R13
+increase R13D = R13
+increase R13 = R13
+increase R14B = R14
+increase R14D = R14
+increase R14 = R14
+increase R15B = R15
+increase R15D = R15
+increase R15 = R15
+increase RBP = RBP
+increase RSP = RSP
 
+shrink :: Register -> S.Type -> Register
+shrink RAX TInt = EAX
+shrink RAX TByte = AL
+shrink RBX TInt = EBX
+shrink RBX TByte = BL
+shrink RCX TInt = ECX
+shrink RCX TByte = CL
+shrink RDX TInt = EDX
+shrink RDX TByte = DL
+shrink RSI TInt = ESI
+shrink RSI TByte = SIL
+shrink RDI TInt = EDI
+shrink RDI TByte = DIL
+shrink R8 TInt = R8D
+shrink R8 TByte = R8B
+shrink R9 TInt = R9D
+shrink R9 TByte = R9B
+shrink R10 TInt = R10D
+shrink R10 TByte = R10B
+shrink R11 TInt = R11D
+shrink R11 TByte = R11B
+shrink R12 TInt = R12D
+shrink R12 TByte = R12B
+shrink R13 TInt = R13D
+shrink R13 TByte = R13B
+shrink R14 TInt = R14D
+shrink R14 TByte = R14B
+shrink R15 TInt = R15D
+shrink R15 TByte = R15B
+shrink x TRef = x
+shrink reg typ = shrink typ (increase reg)
+
+getRegisterSize :: Register -> S.Type
+getRegisterSize EAX = IntT
+getRegisterSize AL = ByteT
+getRegisterSize EBX = IntT
+getRegisterSize BL = ByteT
+getRegisterSize ECX = IntT
+getRegisterSize CL = ByteT
+getRegisterSize EDX = IntT
+getRegisterSize DL = ByteT
+getRegisterSize ESI = IntT
+getRegisterSize SIL = ByteT
+getRegisterSize EDI = IntT
+getRegisterSize DIL = ByteT
+getRegisterSize R8D = IntT
+getRegisterSize R8B = ByteT
+getRegisterSize R9D = IntT
+getRegisterSize R9B = ByteT
+getRegisterSize R10D = IntT
+getRegisterSize R10B = ByteT
+getRegisterSize R11D = IntT
+getRegisterSize R11B = ByteT
+getRegisterSize R12D = IntT
+getRegisterSize R12B = ByteT
+getRegisterSize R13D = IntT
+getRegisterSize R13B = ByteT
+getRegisterSize R14D = IntT
+getRegisterSize R14B = ByteT
+getRegisterSize R15D = IntT
+getRegisterSize R15B = ByteT
+getRegisterSize x = Reference
 
 data Val = 
     VConst Integer
@@ -115,9 +222,21 @@ instance Show Val where
   show (VLoc i) = "$+" ++ show i
   show (VLab s) = s
 
-
+isRegisterValue :: Val -> Bool 
 isRegisterValue (VReg _) = True
 isRegisterValue _ = False
 
+isMemoryValue :: Val -> Bool 
 isMemoryValue (VMem _ _ _ _) = True
 isMemoryValue _ = False
+
+shrinkRegisterValue :: S.Type -> Val -> Val 
+shrinkRegisterValue typ (Reg reg) = Reg (shrink typ reg)
+shrinkRegisterValue _ x = x
+
+increaseRegisterValue :: Val -> Val 
+increaseRegisterValue (Reg reg) = Reg (increase reg)
+increaseRegisterValue x = x
+
+getRegisterValueSize :: Val -> S.Type
+getRegisterValueSize (Reg reg) = getRegisterSize reg
