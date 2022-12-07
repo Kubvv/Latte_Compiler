f7c03e72f5c25c5a290e8e52b47317c3615a8ed0
Kubv <jakub.grondziowski@gmail.com>
2022-12-07 17:10:38 +0100

    docs(backend): document liveness check code

--- src/backend/LivenessCheck.hs
+++ src/backend/LivenessCheck.hs
@@ -9,6 +9,9 @@ import Control.Monad.State
 import SsaData
 import LivenessCheckData
 
+-- Finds the line of a given block (represented as list) with label name 
+-- matching the second argument findLabelLine should always return Just line
+-- as all labels have been correctly generated during ssa translation
 findLabelLine :: [(Integer, Stmt)] -> String -> Maybe Integer
 findLabelLine [] _ = Nothing
 findLabelLine ((line, PutLab x):stmts) s 
@@ -16,6 +19,9 @@ findLabelLine ((line, PutLab x):stmts) s
   | otherwise = findLabelLine stmts s
 findLabelLine (stmt:stmts) s = findLabelLine stmts s
 
+-- Links the given list of statements by adding connections between labels and jumps
+-- that refer to those labels. If statement is not a jump, link it to the next statement.
+-- If statement is a return, don't link it to anything (Last instruction of the block)
 linkProgramFlow :: [(Integer, Stmt)] -> (Integer, Stmt) -> (Integer, Stmt, [Integer])
 linkProgramFlow prog (line, stmt@(Jmp s)) =
   (line, stmt, [sline])
@@ -30,58 +36,70 @@ linkProgramFlow _ (line, stmt@(RetV)) =
 linkProgramFlow _ (line, stmt) =
   (line, stmt, [line + 1])
 
+-- Adds the set of live variables before and after each statement
 addLivenessSets :: (Integer, Stmt, [Integer]) -> (Integer, Stmt, [Integer], Set String, Set String)
 addLivenessSets (line, stmt, link) = (line, stmt, link, S.empty, S.empty)
 
+-- Removes unneccessary data from statements (line and links) and swaps sets to list for easier access
 cleanStmtFlow :: (Integer, Stmt, [Integer], Set String, Set String) -> (Stmt, [String], [String])
-cleanStmtFlow (line, stmt, link, from, to) = (stmt, S.toList from, S.toList to) --TODO Think about names from & to
+cleanStmtFlow (line, stmt, link, before, after) = (stmt, S.toList before, S.toList after) --TODO Think about names from & to
 
+-- Prepares the given statements by adding line numbers and before & after sets, then the prepared
+-- statements are used in the process of building the before & after sets
 checkLiveness :: [Stmt] -> [(Stmt, [String], [String])]
 checkLiveness stmts =
-  P.map cleanStmtFlow (buildFromToSets prepStmts)
+  P.map cleanStmtFlow (buildBefAftSets prepStmts)
     where
       prepStmts = P.map (addLivenessSets . (linkProgramFlow lineStmts)) lineStmts
-      lineStmts = zip [1..] stmts
+      lineStmts = zip [1..] stmts -- Adds line numbers to all statements
 
-buildFromToSets :: [FromToStmt] -> [FromToStmt]
-buildFromToSets stmts =
-  if modstmts == stmts then modstmts else buildFromToSets modstmts
+-- Iteratively adjusts the before and after sets until it reaches a fixed point
+buildBefAftSets :: [BefAftStmt] -> [BefAftStmt]
+buildBefAftSets stmts =
+  if modstmts == stmts then modstmts else buildBefAftSets modstmts
     where
-      modstmts = P.map (createFromToSet stmts) stmts
+      modstmts = P.map (createBefAftSet stmts) stmts
 
-createFromToSet :: [FromToStmt] -> FromToStmt -> FromToStmt
-createFromToSet stmts (line, stmt, link, from, to) =
-  (line, stmt, link, modfrom, modto)
+-- Performs one step of iteration during which before and after sets are modified
+-- in the way described during the lecture: after set is created by a union of all
+-- before sets of next statements, whereas before set is created from a union of
+-- used variables and a difference of after set and the assigned to variable name
+createBefAftSet :: [BefAftStmt] -> BefAftStmt -> BefAftStmt
+createBefAftSet stmts (line, stmt, link, before, after) =
+  (line, stmt, link, modbefore, modafter)
     where
-      linkers = findLinkers link stmts []
-      modfrom = S.union (fromList $ getStmtVars stmt) (S.difference to (getAssignmentStr stmt))
-      modto = S.unions linkers
+      linkersSets = findLinkers link stmts []
+      modbefore = S.union (fromList $ getStmtVars stmt) (S.difference after (S.fromList $ getAssignmentStr stmt))
+      modafter = S.unions linkersSets
 
-getAssignmentStr :: Stmt -> Set String
-getAssignmentStr (Decl _ s _) = S.singleton s
-getAssignmentStr (Ass _ (LVar s) _) =  S.singleton s
-getAssignmentStr _ = S.fromList []
-
-
-createVarRanges :: [LiveStmtVars] -> [VarRange]
+-- Converts the liveness statements with before and after sets into a list of 
+-- all used variables and their liveness range. Resulting list may have
+-- repeating variable names as some variables may be alive in two different ranges
+createVarRanges :: [LiveStmt] -> [VarRange]
 createVarRanges swls = varRanges res
   where
     res = execState (varRangesController swls) emptyLState
 
-varRangesController :: [LiveStmtVars] -> LivenessMonad ()
+varRangesController :: [LiveStmt] -> LivenessMonad ()
 varRangesController swls =
   do
     mapM_ varRangesStmt swls
 
-varRangesStmt :: LiveStmtVars -> LivenessMonad () --TODO zrozumieć
-varRangesStmt (stmt, from, to) =
+-- For each statement varRangesStmt updates the state by modifying
+-- the variable liveness ranges and increasing the line counter by 1
+varRangesStmt :: LiveStmt -> LivenessMonad ()
+varRangesStmt (stmt, before, _) =
   do
     store <- get
     let l = line store
     modify $ incrLStateCounter
-    let modVars = P.foldl (analizeVarRange l) (varRanges store) from
+    let modVars = P.foldl (analizeVarRange l) (varRanges store) before
     modify $ putVarRanges modVars
 
+-- Given a line number, current variable liveness ranges and a variable name,
+-- analizeVarRange modifies the current ranges by either extending the already
+-- present var range (If the end of the range is just before the line number)
+-- or by creating a new range for this particular variable
 analizeVarRange :: Integer -> [VarRange] -> String -> [VarRange]
 analizeVarRange line [] v2 = [(v2, line, line)]
 analizeVarRange line ((v1, begin, end):vrs) v2 =
--- src/backend/LivenessCheckData.hs
+++ src/backend/LivenessCheckData.hs
@@ -6,36 +6,85 @@ import Data.Set as S
 
 import SsaData
 
---TODO NAME
-type FromToStmt = (Integer, Stmt, [Integer], Set String, Set String)
-type LiveStmtVars = (Stmt, [String], [String])
-type VarRange = (String, Integer, Integer)
+-- BefAftStmt is a data structure that holds extra statement information,
+-- such as line number, next stmt's line (reffered to as link) and sets of 
+-- live variables before and after the statement. From now on I'll refer to 
+-- those sets as 'before' and 'after'
+type BefAftStmt = (Integer, Stmt, [Integer], Set String, Set String)
 
-getFTSLine :: FromToStmt -> Integer
-getFTSLine (line, _, _, _, _) = line
+-- Getters
+getBASLine :: BefAftStmt -> Integer
+getBASLine (line, _, _, _, _) = line
 
-getFTSLink :: FromToStmt -> [Integer]
-getFTSLink (_, _, link, _, _) = link
+getBASLink :: BefAftStmt -> [Integer]
+getBASLink (_, _, link, _, _) = link
 
-getFTSFrom :: FromToStmt -> Set String
-getFTSFrom (_, _, _, from, _) = from
+getBASBefore :: BefAftStmt -> Set String
+getBASBefore (_, _, _, before, _) = before
 
-findLinkers :: [Integer] -> [FromToStmt] -> [Set String] -> [Set String]
+-- findLinkers is a function that looks for before sets in statements
+-- described by their line numbers (first argument)
+findLinkers :: [Integer] -> [BefAftStmt] -> [Set String] -> [Set String]
 findLinkers _ [] acc = acc
 findLinkers link (fts:ftss) acc =
-  if getFTSLine fts `elem` link then
-    findLinkers link ftss (getFTSFrom fts : acc)
+  if getBASLine fts `elem` link then
+    findLinkers link ftss (getBASBefore fts : acc)
   else
     findLinkers link ftss acc
 
+-- LiveStmt is similiar to BefAftStmt, however it doesn't contain line numebrs
+-- and link to next statements
+type LiveStmt = (Stmt, [String], [String])
+
+-- Getters
+getLivenessStmt :: LiveStmt -> Stmt
+getLivenessStmt (s, _, _) = s
+
+-- Check if a given string matches a variable name in declaration statement
+isLivenessDeclName :: String -> LiveStmt -> Bool
+isLivenessDeclName s1 ((Decl _ s2 _), _, _) | s1 == s2 = True
+isLivenessDeclName _ _ = False
+
+-- Var range describes a singular liveness range of a single variable
+type VarRange = (String, Integer, Integer)
+
+-- Getters
+getVarRangeName :: VarRange -> String
+getVarRangeName (s, _, _) = s
+
+getVarRangeFrom :: VarRange -> Integer
+getVarRangeFrom (_, from, _) = from
+
+getVarRangeAfter :: VarRange -> Integer
+getVarRangeAfter (_, _, after) = after
+
+-- Checks if a given name of VarRange is not a part of a given string list
+isNotVarRangeNames :: [String] -> VarRange -> Bool
+isNotVarRangeNames strs (s, _, _) | s `notElem` strs = True
+isNotVarRangeNames _ _ = False
+
+-- Checks if a given string and name of VarRange matches
+isVarRangeName :: String -> VarRange -> Bool 
+isVarRangeName x (s, _, _) | x == s = True
+isVarRangeName _ _ = False 
+
+-- Transofrms the VarRange data type to a new pair form that allows for 
+-- easier grouping later on
+createGroups :: [VarRange] -> [(String, [(Integer, Integer)])]
+createGroups [] = []
+createGroups ((s, from, to):vrs) = (s, [(from, to)]) : createGroups vrs
+
+-- LivenessState is solely used in LiveStmt to VarRanges 
+-- conversion function
 data LivenessState = LState {
-  varRanges :: [VarRange],
-  line :: Integer
+  varRanges :: [VarRange], -- Holds all var liveness ranges discovered so far
+  line :: Integer -- Holds the current line number of the analyzed statement
 }
 
 emptyLState :: LivenessState
 emptyLState = LState [] 1
 
+-- Puts
 incrLStateCounter :: LivenessState -> LivenessState
 incrLStateCounter (LState ran line) = LState ran (line + 1) 
 
