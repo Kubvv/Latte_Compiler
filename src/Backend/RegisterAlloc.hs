d9a67b6a3e6e2837b5388c96dc7dd9f637e0273b
Kubv <jakub.grondziowski@gmail.com>
2022-12-19 19:57:01 +0100

    docs(backend): comment all RegisterAlloc functions

--- src/Backend/RegisterAlloc.hs
+++ src/Backend/RegisterAlloc.hs
@@ -10,6 +10,10 @@ import LivenessCheckData
 import Assembler
 import RegisterAllocData
 
+-- Gets the type of a given variable (represented by a string) by first
+-- looking for its declaration in the live statements list. If found, the type
+-- is extracted from the declaration statement, otherwise the type of the variable
+-- is looked for in the argument list 
 getTypeFromLiveness :: [LiveStmt] -> [Arg] -> String -> Type
 getTypeFromLiveness liveness args s =
   if P.null found then
@@ -18,14 +22,23 @@ getTypeFromLiveness liveness args s =
     fromJust $ getDeclTyp $ getLivenessStmt $ head found
       where found = P.filter (isLivenessDeclName s) liveness
 
+-- Modifies ranges of a register (given as argument) in a RegisterRange list, by
+-- allocating all variables from a given list of variable ranges 
 modifyRegStateRange :: Register -> [VarRange] -> RegisterRange -> RegisterRange
 modifyRegStateRange r1 xrange ((r2, range2):rgs)
   | r1 == r2 = (r1, findRanges xrange range2) : rgs
   | otherwise = (r2, range2) : modifyRegStateRange r1 xrange rgs
 
+-- Finds all ranges in a given list of ranges that can allocate a given list of
+-- VarRanges. The newly updated ranges list with allocated variables is returned
 findRanges :: [VarRange] -> [Range] -> [Range]
 findRanges xrange range = P.foldl findSingularRange range xrange
 
+-- Finds an unoccupied range in a given list of ranges that could fit a given VarRange
+-- and when found, updates this given range in the list by dissecting it into 3 parts: 
+-- pre-alloc and post-alloc, which remain unoccupied and a new range occupied by a 
+-- variable from VarRange argument. This function is called only after we made sure
+-- that there exists a space for that variable
 findSingularRange :: [Range] -> VarRange -> [Range]
 findSingularRange ((Nothing, from1, to1):rgs) (x, from2, to2) | from1 <= from2 && to1 >= to2 =
   concat [pre, [(Just x, from2, to2)], post, rgs]
@@ -34,25 +47,50 @@ findSingularRange ((Nothing, from1, to1):rgs) (x, from2, to2) | from1 <= from2 &
       post = [(Nothing, to2 + 1, to1) | to1 > to2]
 findSingularRange (rg:rgs) x = rg : findSingularRange rgs x
 
+-- Modifies a given Register State data so that it mirrors the fact of allocating a given 
+-- VarRanges xrange to register r and putting the variable with name x to ValMap to mark
+-- the fact that this variable can be found under a scaled register r (scaled by type of variable)  
 putToRegister :: [LiveStmt] -> [Arg] -> String -> Register -> [VarRange] -> RegisterState -> RegisterState
 putToRegister liveness args x r xrange (RegState range vm stc) =
   RegState (modifyRegStateRange (increase r) xrange range) (modifyRegStateMap x (VReg (shrink r typ)) vm) stc
     where typ = getTypeFromLiveness liveness args x
 
+-- For a given list of variables and their liveness, find a register in
+-- the current Register state (from RegisterRange table) that could allocate all
+-- of the variables from the list. If no register could allocate it, return Nothing
 getFreeRegister :: [VarRange] -> RegisterState -> Maybe Register
 getFreeRegister xrange (RegState range _ _) =
   if P.null res then Nothing else Just (head res)
     where
       res = P.map fst (P.filter (isRegSpace xrange) range)
 
+-- For a given list of variables, check if all of them can be allocated
+-- by a given register and its occupancy ranges (allocation definition can be found
+-- in the isSingularRegSpace function)
 isRegSpace :: [VarRange] -> (Register, [Range]) -> Bool
 isRegSpace xrange (r, ranges) = all (isSingularRegSpace ranges) xrange
 
+-- For a given variable and its range where it is alive (VarRange), check if some range of 
+-- some register in given list of ranges could allocate it (It is free in a range 
+-- that completely covers the liveness range of a given variable). If such range exists,
+-- return true, otherwise return false.
 isSingularRegSpace :: [Range] -> VarRange -> Bool
 isSingularRegSpace [] _ = False
 isSingularRegSpace ((Nothing, from1, to1):rgs) (_, from2, to2) | from1 <= from2 && to1 >= to2 = True
 isSingularRegSpace (_:rgs) x = isSingularRegSpace rgs x
 
+-- Updates a given Register State by putting the already established variable locations in 
+-- assembly code (The variable is always an argument of a function). If the argument variable
+-- is stored in a register, the RegisterRange and ValMap is updated in the RegisterState to
+-- reflect that fact (using a putToRegister function). If the argument variable is stored
+-- in memory, then we check if it is alive anywhere in the list of VarRanges. 
+-- If it is and there is a free register that can hold this variable for its alive period,
+-- then the Register State is modified to reflect the fact that the argument reserves
+-- the register for its alive period. The ValMap is updated as well to reflect the fact that
+-- the argument is stored under a memory address
+-- If it is but there isn't a free register that can hold it for its duration of being alive,
+-- we only update the ValMap to reflect the fact that the argument is stored under a memory address. 
+-- If it isn't alive anywhere it is ignored
 putCurrRanges :: [LiveStmt] -> [Arg] -> [VarRange] -> RegisterState -> (String, [AVal]) -> RegisterState
 putCurrRanges liveness args vran rstate (x, [VReg r]) =
   putToRegister liveness args x r (P.filter (isVarRangeName x) vran) rstate
@@ -61,40 +99,57 @@ putCurrRanges liveness args vran rstate@(RegState range vm stc) (x, [val]) =
     Nothing -> modrstate
     Just r -> putToRegister liveness args x r found modrstate
     where
-      found = P.filter (isVarRangeName x) vran
+      found = P.filter (isVarRangeName x) vran -- Get all ranges of that variable
       modrstate = if P.null found then rstate else RegState range (modifyRegStateMap x val vm) stc
 
-modifyRegStateMap :: String -> AVal -> ValMap -> ValMap
-modifyRegStateMap x v [] = [(x, [v])]
-modifyRegStateMap x1 v ((x2, vs):vms)
-  | x1 == x2 = (x2, v : vs) : vms
-  | otherwise = (x2, vs) : modifyRegStateMap x1 v vms
-
+-- For a given list of liveness variable ranges, merge all entires that describe
+-- the same variable (first element of xrange). The variable ranges were merged to a list,
+-- so a final form of the result is a list of pairs of variables, and all their ranges when
+-- they're alive. Grouping was done using a set
 mergeSameVars :: [VarRange] -> [(String, [(Integer, Integer)])]
 mergeSameVars xrange = toList $ fromListWith (++) (createGroups xrange)
 
+-- Updates a given Register State by putting a new variable described by the pair (x, fromto).
+-- If it is possible to store the variable inside a register for all of its alive period, then
+-- the RegisterRange and valMap of given state is updated by putToRegister function. Otherwise,
+-- if it's not possible to find a free register, the ValMap and Stack top is updated using a 
+-- putToMemory function.
 putNewRanges :: [LiveStmt] -> [Arg] -> RegisterState -> (String, [(Integer, Integer)]) -> RegisterState
 putNewRanges liveness args rstate (x, fromto) =
   case getFreeRegister modvar rstate of
-    Nothing -> fillIn liveness args x rstate
+    Nothing -> putToMemory liveness args x rstate
     Just r -> putToRegister liveness args x r modvar rstate
     where modvar = P.map (mergedToVarRange x) fromto
 
-fillIn :: [LiveStmt] -> [Arg] -> String -> RegisterState -> RegisterState
-fillIn liveness args x (RegState ranges vm stc) =
+-- Update a given Register State by putting a variable on top of the stack. ValMap is updated
+-- by adding a new memory location of that variable (The memory location sits on top of the stack). 
+-- The stack top is shifted by the inserted variable size to reflect that a new variable will appear 
+-- there. This function is called only if there is no space in the registers for that variable. 
+putToMemory :: [LiveStmt] -> [Arg] -> String -> RegisterState -> RegisterState
+putToMemory liveness args x (RegState ranges vm stc) =
   RegState ranges (modifyRegStateMap x (VMem RBP Nothing (Just (-stc-size)) (Just typ)) vm) (stc + size)
     where
       typ = getTypeFromLiveness liveness args x
       size = getTypeSize typ
 
+-- Merge a given variable and a range described as (from, to) into
+-- a VarRange type (a triple of variable and it's liveness range)
 mergedToVarRange :: String -> (Integer, Integer) -> VarRange
 mergedToVarRange x (from, to) = (x, from, to)
 
+-- alloc is the main endpoint of the RegisterAlloc file. Its goal is to
+-- create a RegisterState for some block of code, given the current value map
+-- (places where variables are stored), arguments of the function and liveness sets
+-- in betweent the statements. First of all the function creates VarRanges using liveness
+-- sets (VarRange describes the ranges where variables are alive), Secondly it creates
+-- an initial register state. Then the register range is updated with already establised
+-- variables (which are arguments saved in registers and memory). Then all variables from
+-- a given block of live statements are allocated based by their liveness ranges
+-- either in registers or in memory
 alloc :: ValMap -> [Arg] -> [LiveStmt] -> RegisterState
-alloc currvmap args liveness =
-  shiftRegStateStack res 8
-    where
-      varRan = createVarRanges liveness
-      inits = RegState (P.map (initialRegisterRange (length liveness)) modifiableRegisters) [] 8
-      sargs = P.foldl (putCurrRanges liveness args varRan) inits currvmap
-      res = P.foldl (putNewRanges liveness args) sargs (mergeSameVars (P.filter (isNotVarRangeNames (getArgNames args)) varRan))
+alloc currvmap args liveness = shiftRegStateStack res 8
+  where
+    varRan = createVarRanges liveness
+    inits = RegState (P.map (initialRegisterRange (length liveness)) modifiableRegisters) [] 8
+    sargs = P.foldl (putCurrRanges liveness args varRan) inits currvmap
+    res = P.foldl (putNewRanges liveness args) sargs (mergeSameVars (P.filter (isNotVarRangeNames (getArgNames args)) varRan))
--- src/Backend/RegisterAllocData.hs
+++ src/Backend/RegisterAllocData.hs
@@ -4,48 +4,83 @@ import LivenessCheckData
 import Assembler as A
 import QuadruplesData as Q
 
--- Which var occupies register, from, to
+-- Range is a triple that stores an information about occupancy of some register
+-- in some scope, described by two Integers being lines of code. The first argument
+-- is Nothing when the register is free in a given range, and Just x where x is a
+-- variable name when register is occupied 
 type Range = (Maybe String, Integer, Integer)
 
+-- For a given integer and a range (of some register), check if
+-- the range is ocuppied and the given integer falls in the range
 isJustAndInRange :: Integer -> Range -> Bool
 isJustAndInRange i (Just _, from, to) | from <= i && i <= to = True
 isJustAndInRange _ _ = False
 
+-- For a given integer and a range (of some register), check if
+-- the range is unocuppied and the given integer falls in the range
 isNothingAndInRange :: Integer -> Range -> Bool
 isNothingAndInRange i (Nothing, from, to) | from <= i && i <= to = True
 isNothingAndInRange _ _ = False
 
-getArrRangeRegister :: [Range] -> Maybe String
-getArrRangeRegister [(r, _, _)] = r
+-- Getter
+getArrRangeVar :: [Range] -> Maybe String
+getArrRangeVar [(v, _, _)] = v
 
+-- RegisterRange stores a list of all register and their ranges. Since every
+-- register can have many different spans during the execution of code, each
+-- register has a list of many possible ranges
 type RegisterRange = [(Register, [Range])]
 
+-- Create an initial registerRange by assigning no variable for a whole block of code
+-- of length len to a register r
 initialRegisterRange :: Int -> Register -> (Register, [Range])
 initialRegisterRange len r = (r, [(Nothing, 1, fromIntegral len)])
 
+-- For a given Integer that represents code line number and a register with its ranges,
+-- Get a range of that register that is occupied by some variable at the code line number i
 filterJustInRanges :: Integer -> (Register, [Range]) -> [Range]
 filterJustInRanges i (_,rs) = filter (isJustAndInRange i) rs
 
+-- For a given integer that represents code line number and a register with its ranges,
+-- Check if this register has a range when it is unoccupied and the code line falls between
+-- this range
 anyNothingInRanges :: Integer -> (Register, [Range]) -> Bool
 anyNothingInRanges i (_,rs) = any (isNothingAndInRange i) rs
 
+-- Holds a mapping from a variable to assembly values, which basically
+-- tells how can a given variable be described in the x86_64 code
 type ValMap = [(String, [AVal])]
 
+-- For a given variable and its mapping to assembly value, check
+-- if the amount of representations are equal to the given number
 isValueCount :: Int -> (String, [AVal]) -> Bool
 isValueCount count (_,av) = length av == count
 
+-- Get first register value in a given list of assembly values
 getFirstRegister :: [AVal] -> Maybe AVal
 getFirstRegister [] = Nothing
 getFirstRegister (r@VReg {}:avs) = Just r
 getFirstRegister (_:avs) = getFirstRegister avs
 
+-- Get first memory value in a given list of assembly values
 getFirstMemory :: [AVal] -> Maybe AVal
 getFirstMemory [] = Nothing
 getFirstMemory (r@VMem {}:avs) = Just r
 getFirstMemory (_:avs) = getFirstMemory avs
 
+-- For a given string describing a variable name and a ValMap,
+-- modify the mapping of that variable by adding a new assembly value where
+-- the variable is stored. If the entry wasn't found (a variable wasn't stored
+-- anywhere), add a new entry to ValMap with that variable and its assembly value
+modifyRegStateMap :: String -> AVal -> ValMap -> ValMap
+modifyRegStateMap x v [] = [(x, [v])]
+modifyRegStateMap x1 v ((x2, vs):vms)
+  | x1 == x2 = (x2, v : vs) : vms
+  | otherwise = (x2, vs) : modifyRegStateMap x1 v vms
+
 type Arg = (Q.Type, String)
 
+-- Find argument in the list of arguments by its name
 findArg :: [Arg] -> String -> Maybe Arg
 findArg [] _ = Nothing
 findArg (arg@(typ, s1):args) s2 | s1 == s2 = Just arg
@@ -55,11 +90,12 @@ getArgNames :: [Arg] -> [String]
 getArgNames = map snd
 
 data RegisterState = RegState {
-  range :: RegisterRange,
-  vMap :: ValMap,
-  stack :: Integer
+  range :: RegisterRange, -- Holds all registers and what occupies them at every range
+  vMap :: ValMap, -- Holds the mapping from a variable to all assembly location where that var is stored
+  stack :: Integer -- Holds the stack top used when putting variables on the stack (due to lack of space in registers)
 }
 
+-- Setters
 putvm :: ValMap -> RegisterState -> RegisterState
 putvm vm (RegState range _ stc) = RegState range vm stc
 
