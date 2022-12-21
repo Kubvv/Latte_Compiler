80006a3e764694578e10431e30b3e30513a01702
Kubv <jakub.grondziowski@gmail.com>
2022-12-21 18:21:34 +0100

    feat(general): add an option of disabling optimizations

--- src/Main.hs
+++ src/Main.hs
@@ -30,17 +30,19 @@ import Assembler
 import Generator
 import Extractor
 
-unpackArgs :: [String] -> (Bool, Maybe String, Maybe String, Bool) -> (Bool, Maybe String, Maybe String, Bool)
-unpackArgs ("-h":args) (False, o, files, err) = unpackArgs args (True, o, files, err)
-unpackArgs ("-h":args) (True, o, files, err) = unpackArgs args (True, o, files, True)
-unpackArgs ("-o":out:args) (h, Nothing, files, err) = unpackArgs args (h, Just out, files, err)
-unpackArgs ("-o":out:args) (h, _, files, err) = unpackArgs args (h, Nothing, files, True)
-unpackArgs (f:args) (h, o, Nothing, err) = 
+unpackArgs :: [String] -> (Bool, Bool, Maybe String, Maybe String, Bool) -> (Bool, Bool, Maybe String, Maybe String, Bool)
+unpackArgs ("-h":args) (False, n, o, files, err) = unpackArgs args (True, n, o, files, err)
+unpackArgs ("-h":args) (True, n, o, files, err) = unpackArgs args (True, n, o, files, True)
+unpackArgs ("-n":args) (h, True, o, files, err) = unpackArgs args (h, False, o, files, err)
+unpackArgs ("-n":args) (h, False, o, files, err) = unpackArgs args (h, False, o, files, True)
+unpackArgs ("-o":out:args) (h, n, Nothing, files, err) = unpackArgs args (h, n, Just out, files, err)
+unpackArgs ("-o":out:args) (h, n, _, files, err) = unpackArgs args (h, n, Nothing, files, True)
+unpackArgs (f:args) (h, n, o, Nothing, err) = 
   if head f == '-' then
-    (h, o, Nothing, True)
+    (h, n, o, Nothing, True)
   else 
-    unpackArgs args (h, o, Just f, err)
-unpackArgs (f:args) (h, o, _, err) = (h, o, Nothing, True)
+    unpackArgs args (h, n, o, Just f, err)
+unpackArgs (f:args) (h, n, o, _, err) = (h, n, o, Nothing, True)
 unpackArgs [] acc = acc
 
 exitErrWithMessage :: String -> Bool -> IO ()
@@ -53,52 +55,57 @@ exitErrWithMessage err isProgErr =
 exitWithHelpMessage :: IO ()
 exitWithHelpMessage =
   do
-    putStrLn "Compile using following command:"
-    putStrLn "  latc [-h] [-o output_file] file_name.lat"
+    putStrLn "Compile using the following command:"
+    putStrLn "  latc [-h] [-n] [-o output_file] file_name.lat"
     putStrLn "  -h: print help message"
     putStrLn "  -o output_file: output the compiled code to output_file"
     putStrLn "     if not specified, output to the file with the same name as input file"
+    putStrLn "  -n: do not use any major code optimalizations (LCSE,"
+    putStrLn "     Value propagation, post compilation clean)"
     putStrLn "  file_name.lat: to be compiled file written in latte"
     exitSuccess
 
-syntaxCheck :: String -> String -> IO ()
-syntaxCheck out file = 
+syntaxCheck :: String -> Bool -> String -> IO ()
+syntaxCheck out optim file = 
   do
     x <- doesFileExist file
     unless x $
       exitErrWithMessage "Input path is not a file or does not exist" False
     f <- readFile file
     case pProgram $ myLexer f of
-      Right t -> runFrontCheck out t
+      Right t -> runFrontCheck out optim t
       Left err -> exitErrWithMessage err True
 
-runFrontCheck :: String -> Abs.Program -> IO ()
-runFrontCheck out prog = 
+runFrontCheck :: String -> Bool -> Abs.Program -> IO ()
+runFrontCheck out optim prog = 
   do
     val <- runExceptT $ checkTypes (convertProg prog) 
     case val of
-      Right (t, cs) -> runOptimizer out t cs 
+      Right (t, cs) -> runOptimizer out optim t cs 
       Left err -> exitErrWithMessage (show err) True
 
-runOptimizer :: String -> A.Program -> [T.Class] -> IO ()
-runOptimizer out prog cs =
+runOptimizer :: String -> Bool -> A.Program -> [T.Class] -> IO ()
+runOptimizer out optim prog cs =
   do
     val <- runExceptT $ optimize prog
     case val of
-      Right t -> runInterTranslate out t cs
+      Right t -> runInterTranslate out optim t cs
       Left err -> exitErrWithMessage (show err) True
 
-runInterTranslate :: String -> A.Program -> [T.Class] -> IO ()
-runInterTranslate out prog cs =
+runInterTranslate :: String -> Bool -> A.Program -> [T.Class] -> IO ()
+runInterTranslate out optim prog cs =
   do
     inter <- interTranslate prog cs
-    revampedInter <- revamp inter
-    runAssemblyBuild out revampedInter
+    if optim then do 
+      revampedInter <- revamp inter
+      runAssemblyBuild out optim revampedInter
+    else
+      runAssemblyBuild out optim inter
 
-runAssemblyBuild :: String -> S.Program -> IO ()
-runAssemblyBuild out prog =
+runAssemblyBuild :: String -> Bool -> S.Program -> IO ()
+runAssemblyBuild out optim prog =
   do
-    final <- generate prog
+    final <- generate prog optim
     extract out final
     hPutStrLn stderr "OK"
     exitSuccess
@@ -107,7 +114,7 @@ main :: IO ()
 main = 
   do
     args <- getArgs
-    let (help, rawOut, file, err) = unpackArgs args (False, Nothing, Nothing, False)
+    let (help, optim, rawOut, file, err) = unpackArgs args (False, True, Nothing, Nothing, False)
     when err $
       exitErrWithMessage "Wrong format of arguments. Write -h to see help." False
     when help
@@ -116,4 +123,4 @@ main =
       exitErrWithMessage "No files to compile. Write -h to see help." False
     let f = fromMaybe "" file
     let out = if isNothing rawOut then fst $ splitExtension f else fromMaybe "" rawOut
-    syntaxCheck out f
+    syntaxCheck out optim f
